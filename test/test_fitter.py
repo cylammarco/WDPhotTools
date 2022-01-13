1ff2fe3251bd808d31ba48b082f73986543c9258
Ka Wai Yuen <kawai.yuen@icloud.com>
2022-01-13 01:00:36 +0000

    interp_reddening_tested

--- test/test_fitter.py
+++ test/test_fitter.py
@@ -577,3 +577,7 @@ def test_fitting_logg_and_Mbol_red_emcee():
                       np.array([9.962, 7.5]),
                       rtol=1e-03,
                       atol=1e-03).all()
+
+def test_interp_reddening():
+    ftr.interp_reddening(filters=['G3', 'G3_BP', 'G3_RP', 'FUV', 'NUV'],
+                         interpolated=True)
