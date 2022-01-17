bc807c70e2a31d1a19c507014c1d183a9443940e
Ka Wai Yuen <kawai.yuen@icloud.com>
2022-01-17 11:45:20 +0000

    17jan_trial1

--- test/test_fitter.py
+++ test/test_fitter.py
@@ -608,14 +608,15 @@ def test_chi2_minimization_red_interpolated():
                       rtol=1e-03,
                       atol=1e-03).all()
 
-# Testing the _chi2_minimization_distance_red_interpolated() by YKW on 16Jan2022
+# Testing the _chi2_minimization_distance_red_interpolated() by YKW on 17Jan2022
 def test_chi2_minimization_distance_red_interpolated():
     mags = np.array([10.882, 10.853, 10.946, 11.301, 11.183])
     mags = mags + extinction_interpolated
     ftr.fit(filters=['G3', 'G3_BP', 'G3_RP', 'FUV', 'NUV'],
             mags=mags,
             mag_errors=[0.1, 0.1, 0.1, 0.1, 0.1],
-            independent=['Mbol', 'logg'],
+            logg=7.5,
+            independent=['Mbol'],
             method='emcee',
             initial_guess=[10.0, 7.5],
             refine_bounds=[0.1, 99.9],
@@ -626,19 +627,16 @@ def test_chi2_minimization_distance_red_interpolated():
                       folder='test_output',
                       filename='test_chi2_minimization_distance_red_interpolated',
                       ext='png')
-    assert np.isclose(ftr.results['H'].x,
-                      np.array([9.962, 7.5]),
-                      rtol=1e-03,
-                      atol=1e-03).all()
 
-# Testing the _chi2_minimization_distance_red_filter_fixed_logg() by YKW on 16Jan2022
+# Testing the _chi2_minimization_distance_red_filter_fixed_logg() by YKW on 17Jan2022
 def test_chi2_minimization_distance_red_filter_fixed_logg():
     mags = np.array([10.882, 10.853, 10.946, 11.301, 11.183])
     mags = mags + extinction
     ftr.fit(filters=['G3', 'G3_BP', 'G3_RP', 'FUV', 'NUV'],
             mags=mags,
             mag_errors=[0.1, 0.1, 0.1, 0.1, 0.1],
-            independent=['Mbol', 'logg'],
+            logg=7.5,
+            independent=['Mbol'],
             method='emcee',
             initial_guess=[10.0, 7.5],
             refine_bounds=[0.1, 99.9],
@@ -648,7 +646,3 @@ def test_chi2_minimization_distance_red_filter_fixed_logg():
                       folder='test_output',
                       filename='test_chi2_minimization_distance_red_filter_fixed_logg',
                       ext='png')
-    assert np.isclose(ftr.results['H'].x,
-                      np.array([9.962, 7.5]),
-                      rtol=1e-03,
-                      atol=1e-03).all()
