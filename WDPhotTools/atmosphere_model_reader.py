fixed a bug with array of nan by setting dtype from object to float64. Changed tests precision requirement.

WDPhotTools/atmosphere_model_reader.py
@@ -446,7 +446,7 @@ class AtmosphereModelReader(object):
 
                 arg_1 = np.log10(arg_1)
 
-            if interpolator.upper() == "CT":
+            if interpolator.lower() == "ct":
 
                 # Interpolate with the scipy CloughTocher2DInterpolator
                 _atmosphere_interpolator = CloughTocher2DInterpolator(
@@ -469,7 +469,7 @@ class AtmosphereModelReader(object):
 
                     return _atmosphere_interpolator(x0, x1)
 
-            elif interpolator.upper() == "RBF":
+            elif interpolator.lower() == "ct":
 
                 # Interpolate with the scipy RBFInterpolator
                 _atmosphere_interpolator = RBFInterpolator(
@@ -530,7 +530,7 @@ class AtmosphereModelReader(object):
                         _x1 = np.log10(_x1)
 
                     return _atmosphere_interpolator(
-                        np.asarray([_x0, _x1], dtype="object").T.reshape(
+                        np.array([_x0, _x1], dtype="object").T.reshape(
                             length0, 2
                         )
                     )

WDPhotTools/cooling_model_reader.py
@@ -2084,9 +2084,9 @@ class CoolingModelReader(object):
         kwargs_for_CT={},
     ):
         """
-        Compute the callable CloughTocher2DInterpolator of the cooling time
-        of WDs. It needs to use float64 or it runs into float-point error
-        at very faint lumnosity.
+        Compute the callable CloughTocher2DInterpolator taking (logL, m) and
+        returning the cooling time of the WDs. It needs to use float64 or it
+        runs into float-point error at very faint lumnosity.
 
         Parameters
         ----------
@@ -2265,7 +2265,7 @@ class CoolingModelReader(object):
         }
         _kwargs_for_RBF.update(**kwargs_for_RBF)
 
-        if interpolator.upper() == "CT":
+        if interpolator.lower() == "ct":
 
             # Interpolate with the scipy CloughTocher2DInterpolator
             self.cooling_interpolator = CloughTocher2DInterpolator(
@@ -2274,7 +2274,7 @@ class CoolingModelReader(object):
                 **_kwargs_for_CT,
             )
 
-        elif interpolator.upper() == "RBF":
+        elif interpolator.lower() == "rbf":
 
             # Interpolate with the scipy RBFInterpolator
             _cooling_interpolator = RBFInterpolator(
@@ -2290,18 +2290,26 @@ class CoolingModelReader(object):
 
             def cooling_interpolator(x0, x1):
 
-                _x0 = np.asarray(x0)
-                _x1 = np.asarray(x1)
+                _x0 = np.array(x0)
+                _x1 = np.array(x1)
+
+                if (_x0.size == 1) & (_x1.size > 1):
+
+                    _x0 = np.repeat(_x0, _x1.size)
+
+                if (_x1.size == 1) & (_x0.size > 1):
+
+                    _x0 = np.repeat(_x1, _x0.size)
 
                 _x0[_x0 < lum_min] = lum_min
                 _x0[_x0 > lum_max] = lum_max
                 _x1[_x1 < mass_min] = mass_min
                 _x1[_x1 > mass_max] = mass_max
 
-                length0 = len(x0)
+                length0 = x0.size
 
                 return _cooling_interpolator(
-                    np.asarray([_x0, _x1], dtype="object").reshape(length0, 2)
+                    np.array([_x0, _x1], dtype="object").T.reshape(length0, 2)
                 )
 
             self.cooling_interpolator = cooling_interpolator
@@ -2320,7 +2328,7 @@ class CoolingModelReader(object):
 
         finite_mask = np.isfinite(self.dLdt)
 
-        if interpolator.upper() == "CT":
+        if interpolator.lower() == "ct":
 
             self.cooling_rate_interpolator = CloughTocher2DInterpolator(
                 (
@@ -2331,7 +2339,7 @@ class CoolingModelReader(object):
                 **_kwargs_for_CT,
             )
 
-        elif interpolator.upper() == "RBF":
+        elif interpolator.lower() == "rbf":
 
             # Interpolate with the scipy RBFInterpolator
             _cooling_rate_interpolator = RBFInterpolator(
@@ -2356,6 +2364,14 @@ class CoolingModelReader(object):
                 _x0 = np.asarray(x0)
                 _x1 = np.asarray(x1)
 
+                if (_x0.size == 1) & (_x1.size > 1):
+
+                    _x0 = np.repeat(_x0, _x1.size)
+
+                if (_x1.size == 1) & (_x0.size > 1):
+
+                    _x0 = np.repeat(_x1, _x0.size)
+
                 _x0[_x0 < lum_min] = lum_min
                 _x0[_x0 > lum_max] = lum_max
                 _x1[_x1 < mass_min] = mass_min
@@ -2364,7 +2380,9 @@ class CoolingModelReader(object):
                 length0 = len(x0)
 
                 return _cooling_rate_interpolator(
-                    np.asarray([_x0, _x1], dtype="object").reshape(length0, 2)
+                    np.asarray([_x0, _x1], dtype="object").T.reshape(
+                        length0, 2
+                    )
                 )
 
             self.cooling_rate_interpolator = cooling_rate_interpolator

WDPhotTools/fitter.py
@@ -1083,7 +1083,7 @@ class WDfitter(AtmosphereModelReader):
         nsteps=1000,
         nburns=100,
         progress=True,
-        refine=True,
+        refine=False,
         refine_bounds=[5.0, 95.0],
         kwargs_for_RBF={},
         kwargs_for_CT={},
@@ -1256,10 +1256,6 @@ class WDfitter(AtmosphereModelReader):
             if len(initial_guess) == len(independent):
 
                 initial_guess = initial_guess + [50.0]
-
-        if distance is np.inf:
-
-            distance = None
 
         # Reuse the interpolator if instructed or possible
         # The +4 is to account for ['Teff', 'mass', 'Mbol', 'age']
@@ -1897,11 +1893,11 @@ class WDfitter(AtmosphereModelReader):
 
                     print("Refining")
 
-                    if distance is None:
+                    _initial_guess = np.percentile(
+                        self.samples[j], [50], axis=0
+                    )
 
-                        _initial_guess = np.percentile(
-                            self.samples[j], [50], axis=0
-                        )
+                    if distance is None:
 
                         # setting distance to infinity so that it will be
                         # turned back to None after the line appending to the
@@ -1916,7 +1912,7 @@ class WDfitter(AtmosphereModelReader):
                             independent=independent,
                             reuse_interpolator=True,
                             method="minimize",
-                            distance=np.inf,
+                            distance=None,
                             distance_err=None,
                             initial_guess=_initial_guess,
                             Rv=Rv,
@@ -1938,9 +1934,7 @@ class WDfitter(AtmosphereModelReader):
                             method="minimize",
                             distance=distance,
                             distance_err=distance_err,
-                            initial_guess=np.percentile(
-                                self.samples[j], [50], axis=0
-                            ),
+                            initial_guess=_initial_guess,
                             Rv=Rv,
                             ebv=ebv,
                             kwargs_for_minimize=kwargs,
@@ -2034,14 +2028,20 @@ class WDfitter(AtmosphereModelReader):
                                     ]
                                 )
                                 for i in self.rv
-                            ]
+                            ],
+                            dtype=np.float64,
                         ).reshape(-1)
                         * ebv
                     )
 
                 else:
 
-                    Av = np.array([i(Rv) for i in self.rv]).reshape(-1) * ebv
+                    Av = (
+                        np.array(
+                            [i(Rv) for i in self.rv], dtype=np.float64
+                        ).reshape(-1)
+                        * ebv
+                    )
 
                 Av[np.isnan(Av)] = 0.0
 
@@ -2084,7 +2084,8 @@ class WDfitter(AtmosphereModelReader):
             Set to return the Figure object.
         **kwarg: dict (Default: {
             'quantiles': [0.158655, 0.5, 0.841345],
-            'show_titles': True})
+            'show_titles': True,
+            'range': [0.95] * len(self.fitting_params["independent"],})
             Keyword argument for the corner.corner().
 
         Return

test/test_fitter_ct.py
@@ -44,7 +44,8 @@ extinction_grizyJHK = np.array(
     [A_g, A_r, A_i, A_z, A_y, A_J, A_H, A_Ks]
 ).reshape(-1)
 
-# Fitting Mbol: Yes
+
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: No
@@ -69,7 +70,7 @@ def test_minimize_Teff_logg():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: No
@@ -97,7 +98,7 @@ def test_minimize_Teff():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: Yes
@@ -127,7 +128,7 @@ def test_minimize_Teff_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: Yes (interpolated)
@@ -158,7 +159,7 @@ def test_minimize_Teff_reddening_interpolated():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: Yes
@@ -187,7 +188,7 @@ def test_minimize_Teff_logg_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: Yes (interpolated)
@@ -217,7 +218,7 @@ def test_minimize_Teff_logg_reddening_interpolated():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: No
@@ -242,7 +243,7 @@ def test_minimize_Teff_logg_distance():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: No
@@ -268,7 +269,7 @@ def test_minimize_Teff_distance():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: Yes
@@ -296,7 +297,7 @@ def test_minimize_Teff_distance_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: Yes (interpolated)
@@ -325,7 +326,7 @@ def test_minimize_Teff_distance_reddening_interpolated():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: Yes
@@ -352,7 +353,7 @@ def test_minimize_Teff_logg_distance_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: Yes (interpolated)
@@ -382,7 +383,7 @@ def test_minimize_Teff_logg_distance_reddening_interpolated():
 
 # least_squares
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: No
@@ -409,7 +410,7 @@ def test_lsq_Teff_logg():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: No
@@ -437,7 +438,7 @@ def test_lsq_Teff():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: Yes
@@ -467,11 +468,11 @@ def test_lsq_Teff_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: Yes (interpolated)
-def test_lsq_Teff_reddening():
+def test_lsq_Teff_reddening_interpolated():
     ftr = WDfitter()
     ftr.fit(
         filters=["G3", "G3_BP", "G3_RP", "FUV", "NUV"],
@@ -498,7 +499,7 @@ def test_lsq_Teff_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: Yes
@@ -527,11 +528,11 @@ def test_lsq_Teff_logg_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: Yes (interpolated)
-def test_lsq_Teff_logg_reddening():
+def test_lsq_Teff_logg_reddening_interpolated():
     ftr = WDfitter()
     ftr.fit(
         filters=["G3", "G3_BP", "G3_RP", "FUV", "NUV"],
@@ -557,7 +558,7 @@ def test_lsq_Teff_logg_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: No
@@ -582,7 +583,7 @@ def test_lsq_Teff_logg_distance():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: No
@@ -609,11 +610,11 @@ def test_lsq_Teff_distance():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: Yes
-def test_lsq_Teff_distance_logg():
+def test_lsq_Teff_distance_reddening():
     ftr = WDfitter()
     ftr.fit(
         filters=["G3", "G3_BP", "G3_RP", "FUV", "NUV"],
@@ -637,11 +638,11 @@ def test_lsq_Teff_distance_logg():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: Yes (interpolated)
-def test_lsq_Teff_distance_logg():
+def test_lsq_Teff_distance_reddening_interpolated():
     ftr = WDfitter()
     ftr.fit(
         filters=["G3", "G3_BP", "G3_RP", "FUV", "NUV"],
@@ -666,7 +667,7 @@ def test_lsq_Teff_distance_logg():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: Yes
@@ -693,11 +694,11 @@ def test_lsq_Teff_logg_distance_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: Yes (interpolated)
-def test_lsq_Teff_logg_distance_reddening():
+def test_lsq_Teff_logg_distance_reddening_interpolated():
     ftr = WDfitter()
     ftr.fit(
         filters=["G3", "G3_BP", "G3_RP", "FUV", "NUV"],
@@ -726,7 +727,7 @@ def test_lsq_Teff_logg_distance_reddening():
 #
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: No
@@ -741,9 +742,10 @@ def test_emcee_Teff_logg():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 7.5],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
         distance=10.0,
         distance_err=0.1,
     )
@@ -757,7 +759,7 @@ def test_emcee_Teff_logg():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: No
@@ -772,9 +774,10 @@ def test_emcee_Teff():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
         logg=7.5,
         distance=10.0,
         distance_err=0.1,
@@ -789,7 +792,7 @@ def test_emcee_Teff():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: Yes
@@ -804,9 +807,10 @@ def test_emcee_Teff_reddening():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
         logg=7.5,
         distance=10.0,
         distance_err=0.1,
@@ -823,7 +827,7 @@ def test_emcee_Teff_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: No
 # Reddenning: Yes (interpolated)
@@ -839,9 +843,10 @@ def test_emcee_Teff_reddening_interpolated():
         atmosphere_interpolator="CT",
         extinction_convolved=False,
         initial_guess=[13000.0],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
         logg=7.5,
         distance=10.0,
         distance_err=0.1,
@@ -858,7 +863,7 @@ def test_emcee_Teff_reddening_interpolated():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: Yes
@@ -873,9 +878,10 @@ def test_emcee_Teff_logg_reddening():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 7.5],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
         distance=10.0,
         distance_err=0.1,
         Rv=rv,
@@ -891,7 +897,7 @@ def test_emcee_Teff_logg_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: No
 # Reddenning: Yes (interpolated)
@@ -907,9 +913,10 @@ def test_emcee_Teff_logg_reddening_interpolated():
         extinction_convolved=False,
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 7.5],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
         distance=10.0,
         distance_err=0.1,
         Rv=rv,
@@ -925,7 +932,7 @@ def test_emcee_Teff_logg_reddening_interpolated():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: No
@@ -968,9 +975,10 @@ def test_emcee_Teff_logg_distance():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 7.5, 10.0],
+        refine=False,
         nwalkers=100,
-        nsteps=2000,
-        nburns=200,
+        nsteps=1000,
+        nburns=100,
     )
     ftr.best_fit_params["H"]["Mbol"]
     ftr.best_fit_params["H"]["Teff"]
@@ -982,7 +990,7 @@ def test_emcee_Teff_logg_distance():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: No
@@ -1025,6 +1033,7 @@ def test_emcee_Teff_distance():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 10.0],
+        refine=False,
         nwalkers=100,
         nsteps=1000,
         nburns=100,
@@ -1040,7 +1049,7 @@ def test_emcee_Teff_distance():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: Yes
@@ -1085,6 +1094,7 @@ def test_emcee_Teff_distance_reddening():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 10.0],
+        refine=False,
         nwalkers=100,
         nsteps=1000,
         nburns=100,
@@ -1102,7 +1112,7 @@ def test_emcee_Teff_distance_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: No
 # Fitting distance: Yes
 # Reddenning: Yes (interpolated)
@@ -1151,6 +1161,7 @@ def test_emcee_Teff_distance_reddening_interpolated():
         extinction_convolved=False,
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 10.0],
+        refine=False,
         nwalkers=100,
         nsteps=1000,
         nburns=100,
@@ -1168,7 +1179,7 @@ def test_emcee_Teff_distance_reddening_interpolated():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: Yes
@@ -1213,6 +1224,7 @@ def test_emcee_Teff_logg_distance_reddening():
         method="emcee",
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 7.5, 10.0],
+        refine=False,
         nwalkers=100,
         nsteps=1000,
         nburns=100,
@@ -1229,7 +1241,7 @@ def test_emcee_Teff_logg_distance_reddening():
     )
 
 
-# Fitting Mbol: Yes
+# Fitting Teff: Yes
 # Fitting logg: Yes
 # Fitting distance: Yes
 # Reddenning: Yes
@@ -1278,6 +1290,7 @@ def test_emcee_Teff_logg_distance_reddening_interpolated():
         extinction_convolved=False,
         atmosphere_interpolator="CT",
         initial_guess=[13000.0, 7.5, 10.0],
+        refine=False,
         nwalkers=100,
         nsteps=1000,
         nburns=100,