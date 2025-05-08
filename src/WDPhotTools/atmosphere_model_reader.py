2fa4b37bb60c42d7a49c22b25d794c48d92f7e6b
zerg <anonzerg@proton.me>
2025-05-08 11:18:21 +0330

    removed an extra } typo

--- src/WDPhotTools/atmosphere_model_reader.py
+++ src/WDPhotTools/atmosphere_model_reader.py
@@ -122,7 +122,7 @@ class AtmosphereModelReader(object):
                 r"$H_{\mathrm{MKO}}$",
                 r"$K_{\mathrm{MKO}}$",
                 r"$W_{1}$",
-                r"$W_{2}}$",
+                r"$W_{2}$",
                 r"$W_{3}$",
                 r"$W_{4}$",
                 r"$S_{36}$",
